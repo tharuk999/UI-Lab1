6d4420c3db01ac075a648696764aa51d9fe5c8ae
tharuk999 <tharukran@gmail.com>
2026-01-13 23:38:05 -0500

    fixed invalid inputs in main

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,65 +1,25 @@
 import java.util.Scanner;
 public class Calculator {
-    public static int numLoop(Scanner scanner){
-        try{
-            num1 = scanner.nextInt();
-        } catch (Exception e){
-            numLoop(scanner);
-        }
-        return num1;
-    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // Input
-
-        int num1=0;
+        int num1;
         int num2;
-//        while (true) {
-//            System.out.println("Enter first number:");
-//            try {
-//                num1 = scanner.nextInt();
-//            } catch (Exception e) {
-//                System.out.println("Invalid Input");
-//            }
-//        }
 
-        /*
-        // Source - https://stackoverflow.com/a
-        // Posted by polygenelubricants, modified by community. See post 'Timeline' for change history
-        // Retrieved 2026-01-13, License - CC BY-SA 3.0
-
-        Scanner sc = new Scanner(System.in);
-        int number;
-        do {
-            System.out.println("Please enter a positive number!");
-            while (!sc.hasNextInt()) {
-                System.out.println("That's not a number!");
-                sc.next(); // this is important!
-            }
-            number = sc.nextInt();
-        } while (number <= 0);
-        System.out.println("Thank you! Got " + number);
-
-         */
-        try{
-            num1 = scanner.nextInt();
+        System.out.println("Enter first number:");
+        while (!scanner.hasNextInt()) { //Ensure int is inputted
+            System.out.println("Invalid Input. Try Again...\nEnter frist number:");
+            scanner.next();
         }
-        catch (Exception e){
-            System.out.println("Invalid Input");
-            numLoop(scanner);
-        }
+        num1 = scanner.nextInt();
 
-        while (true) {
-            System.out.println("Enter second number:");
-            try {
-                num2 = scanner.nextInt();
-                break;
-            } catch (Exception e) {
-                System.out.println("Invalid Input");
-                numLoop();
-            }
+        System.out.println("Enter second number:");
+        while (!scanner.hasNextInt()) { //Ensure int is inputted
+            System.out.println("Invalid Input. Try Again...");
+            scanner.next();
         }
+        num2 = scanner.nextInt();
 
         // Operations
         int sum = num1 + num2; //calculate sum of 2 numbers
@@ -73,5 +33,4 @@ public class Calculator {
         System.out.println("Product: " + product);
         System.out.println("Quotient: " + quotient);
     }
-
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,5 +1,9 @@
 import java.util.Scanner;
+import java.io.ByteArrayOutputStream;
 import java.io.ByteArrayInputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -7,24 +11,39 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class CalculatorTest {
+class CalculatorTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
 
     @BeforeEach
     public void setup(){
         Scanner scanner = new Scanner(System.in);
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
 
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"", "123456", "hello world"})
-    public void NonValidStringTest(String candidate){
+    void NonValidStringTest(String candidate){
 
     }
 
 
     @Test
     public void validTest() {
-        assertEquals(1, 1);
+        String[] data = {"4","2"};
+//        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        Calculator.main(data);
+        assertEquals("Enter first number:\nEnter second number:\nSum: 6\nDifference: 2\nProduct: 8\nQuotient: 2", outContent.toString());
+
     }
 
 
